769b30486384fdc255cef5dcc02b20831d89a076
Evgeniia Liasheva <evgenia_lozovik@mail.ru>
2024-03-07 09:59:24 +0300

    message QA.GURU corrected

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -4,7 +4,7 @@ import java.sql.SQLOutput;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello qa.guru!!!!");
+        System.out.println("Hello QA.GURU!");
         System.out.println("It's gonna be a good day :)");
     }
 }
