ce7b4e38cb1ba534295a2560fa37d696ae10021e
Evgeniia Liasheva <153442050+EvgeniiaLV@users.noreply.github.com>
2024-03-07 10:34:51 +0300

    Day message deleted

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -5,6 +5,5 @@ import java.sql.SQLOutput;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello QA.GURU!");
-        System.out.println("It's gonna be a wonderful day :)");
     }
 }
