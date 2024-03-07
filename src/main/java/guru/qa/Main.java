e9e18ab1bd8c64b794ead241c3918d67f7490c2f
Evgeniia Liasheva <evgenia_lozovik@mail.ru>
2024-03-07 09:56:50 +0300

    Status message added

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -1,7 +1,10 @@
 package guru.qa;
 
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello qa.guru!!!!");
+        System.out.println("It's gonna be a good day :)");
     }
 }
